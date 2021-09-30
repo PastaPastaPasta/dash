5a1496ea130d484b5b83d788a13fafc1e3c2303d
Odysseas Gabrielides <odysseas.gabrielides@gmail.com>
2021-09-30 11:17:46 +0300

    Revert "Renamed variable to avoid confusion with Quorum Index"
    This reverts commit 38425ed53edf2cfebff44018aeb86644e85081ce.

--- src/llmq/dkgsession.h
+++ src/llmq/dkgsession.h
@@ -276,7 +276,7 @@ private:
 public:
     CDKGSession(const Consensus::LLMQParams& _params, CBLSWorker& _blsWorker, CDKGSessionManager& _dkgManager) :
         params(_params), blsWorker(_blsWorker), cache(_blsWorker), dkgManager(_dkgManager) {}
-
+        
     bool Init(const CBlockIndex* pQuorumBaseBlockIndex, const std::vector<CDeterministicMNCPtr>& mns, const uint256& _myProTxHash);
 
     size_t GetMyMemberIndex() const { return myIdx; }
