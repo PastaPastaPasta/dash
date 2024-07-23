65505f2c33447c202454626983a1698242b23aed
Ava Chow <github@achow101.com>
2024-07-23 13:36:11 -0400

    Merge bitcoin/bitcoin#30403: test, assumeutxo: Remove resolved todo comments and add new test
    d63ef738001fb69ce04134cc8645dcd1e1cbccd1 test: Add loadtxoutset test with tip on snapshot block (Fabian Jahr)
    c2f86d4bcba290c33ed99383cc76380bb15ba384 test: Remove already resolved assumeutxo todo comments (Fabian Jahr)
    
    Pull request description:
    
      The first commit removes three Todos that have been addressed previously (see commit message for details).
    
      The second message resolves another todo by adding the missing test case. This is a special case of "the tip has more work than the snapshot" where the tip is the same block as the snapshot base block.
    
      Related to #28648.
    
    ACKs for top commit:
      jrakibi:
        ACK [d63ef73](https://github.com/bitcoin/bitcoin/commit/d63ef738001fb69ce04134cc8645dcd1e1cbccd1)
      achow101:
        ACK d63ef738001fb69ce04134cc8645dcd1e1cbccd1
      maflcko:
        ACK d63ef738001fb69ce04134cc8645dcd1e1cbccd1
      alfonsoromanz:
        Re ACK d63ef738001fb69ce04134cc8645dcd1e1cbccd1
    
    Tree-SHA512: 8d5a25fc0b26531db3a9740132694138f2103b7b42eeb1d4a64095bfc901c1372e23601c0855c7def84c8a4e185d10611e4e830c4e479f1b663ae6ed53abb130

--- test/functional/feature_assumeutxo.py
+++ test/functional/feature_assumeutxo.py
@@ -8,22 +8,6 @@ to a hash that has been compiled into dashd.
 
 The assumeutxo value generated and used here is committed to in
 `CRegTestParams::m_assumeutxo_data` in `src/chainparams.cpp`.
-
-## Possible test improvements
-
-Interesting test cases could be loading an assumeutxo snapshot file with:
-
-- TODO: Valid hash but invalid snapshot file (bad coin height or
-      bad other serialization)
-- TODO: Valid snapshot file, but referencing a snapshot block that turns out to be
-      invalid, or has an invalid parent
-
-Interesting starting states could be loading a snapshot when the current chain tip is:
-
-- TODO: An ancestor of snapshot block
-- TODO: The snapshot block
-- TODO: A descendant of the snapshot block
-
 """
 from dataclasses import dataclass
 from decimal import Decimal
@@ -171,7 +155,6 @@ class AssumeutxoTest(BitcoinTestFramework):
     def test_snapshot_with_less_work(self, dump_output_path):
         self.log.info("Test dashd should fail when snapshot has less accumulated work than this node.")
         node = self.nodes[0]
-        assert_equal(node.getblockcount(), FINAL_HEIGHT)
         with node.assert_debug_log(expected_msgs=["[snapshot] activation failed - work does not exceed active chainstate"]):
             assert_raises_rpc_error(-32603, "Unable to load UTXO snapshot", node.loadtxoutset, dump_output_path)
 
@@ -321,6 +304,10 @@ class AssumeutxoTest(BitcoinTestFramework):
         self.log.info(f"Creating a UTXO snapshot at height {SNAPSHOT_BASE_HEIGHT}")
         dump_output = n0.dumptxoutset('utxos.dat', "latest")
 
+        self.log.info("Test loading snapshot when the node tip is on the same block as the snapshot")
+        assert_equal(n0.getblockcount(), SNAPSHOT_BASE_HEIGHT)
+        assert_equal(n0.getblockchaininfo()["blocks"], SNAPSHOT_BASE_HEIGHT)
+        self.test_snapshot_with_less_work(dump_output['path'])
         assert_equal(n0.getblockchaininfo()["blocks"], SNAPSHOT_BASE_HEIGHT)
 
         def check_dump_output(output):
@@ -382,6 +369,8 @@ class AssumeutxoTest(BitcoinTestFramework):
         self.test_snapshot_not_on_most_work_chain(dump_output['path'])
 
         self.log.info(f"Loading snapshot into second node from {dump_output['path']}")
+        # This node's tip is on an ancestor block of the snapshot, which should
+        # be the normal case
         loaded = n1.loadtxoutset(dump_output['path'])
         assert_equal(loaded['coins_loaded'], SNAPSHOT_BASE_HEIGHT)
         assert_equal(loaded['base_height'], SNAPSHOT_BASE_HEIGHT)
