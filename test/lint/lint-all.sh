e66c4b3f9832353df8b516171364648fdb7f1188
pasta <pasta@dashboost.org>
2024-07-28 13:31:14 -0500

    fixes?

--- test/lint/lint-all.sh
+++ test/lint/lint-all.sh
@@ -36,11 +36,11 @@ else
     fi
   done
 
-  if ! parallel --jobs 100% --will-cite --joblog parallel_out.log bash ::: "${SCRIPTS[@]}"; then
+  if ! parallel --jobs 100% --will-cite --joblog /tmp/parallel_out.log bash ::: "${SCRIPTS[@]}"; then
     echo "^---- failure generated"
     EXIT_CODE=1
   fi
-  column -t parallel_out.log && rm parallel_out.log
+  column -t /tmp/parallel_out.log && rm /tmp/parallel_out.log
 fi
 
 exit ${EXIT_CODE}
