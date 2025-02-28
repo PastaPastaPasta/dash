75968c6ee175b6572f5750d2dcae730c34958bb7
Kittywhiskers Van Gogh <63189531+kwvg@users.noreply.github.com>
2025-02-28 12:30:35 +0000

    ci: avoid making copies when collecting logs, misc. script fixes

--- ci/dash/test_integrationtests.sh
+++ ci/dash/test_integrationtests.sh
@@ -64,7 +64,7 @@ if [ "$BASEDIR" != "" ]; then
     for f in $LOGFILES; do
       d2="testlogs/$d/$(dirname $f)"
       mkdir -p $d2
-      cp testdatadirs/$BASEDIR/$d/$f $d2/
+      mv testdatadirs/$BASEDIR/$d/$f $d2/
     done
   done
 fi
