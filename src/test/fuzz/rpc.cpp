282cf5b016857767597baa878ccf40e860ea6c5c
pasta <pasta@dashboost.org>
2025-11-17 10:20:56 -0600

    fix: getaddresstxids

--- src/test/fuzz/rpc.cpp
+++ src/test/fuzz/rpc.cpp
@@ -112,6 +112,7 @@ const std::vector<std::string> RPC_COMMANDS_SAFE_FOR_FUZZING{
     "getaddressbalance",
     "getaddressdeltas",
     "getaddressmempool",
+    "getaddresstxids",
     "getbestblockhash",
     "getblock",
     "getblockchaininfo",
