ba135001d6c328bd9dc45a3136b6dd2bead93281
pasta <pasta@dashboost.org>
2025-11-17 10:09:10 -0600

    add cleardiscouraged to safe rpc

--- src/test/fuzz/rpc.cpp
+++ src/test/fuzz/rpc.cpp
@@ -88,6 +88,7 @@ const std::vector<std::string> RPC_COMMANDS_SAFE_FOR_FUZZING{
     "analyzepsbt",
     "bls",
     "clearbanned",
+    "cleardiscouraged",
     "combinepsbt",
     "combinerawtransaction",
     "converttopsbt",
