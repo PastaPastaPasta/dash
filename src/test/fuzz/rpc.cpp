baa378b528f118e8bca495555418968caa339b41
pasta <pasta@dashboost.org>
2025-11-17 10:36:56 -0600

    fix: getgovernanceinfo

--- src/test/fuzz/rpc.cpp
+++ src/test/fuzz/rpc.cpp
@@ -133,6 +133,7 @@ const std::vector<std::string> RPC_COMMANDS_SAFE_FOR_FUZZING{
     "getconnectioncount",
     "getdescriptorinfo",
     "getdifficulty",
+    "getgovernanceinfo",
     "getindexinfo",
     "getmemoryinfo",
     "getmempoolancestors",
