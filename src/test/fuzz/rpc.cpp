ee8e9d2a1943f27d4ee1046caf58dbf9bfecff47
pasta <pasta@dashboost.org>
2025-11-17 10:15:00 -0600

    fix: getaddressbalance

--- src/test/fuzz/rpc.cpp
+++ src/test/fuzz/rpc.cpp
@@ -109,6 +109,7 @@ const std::vector<std::string> RPC_COMMANDS_SAFE_FOR_FUZZING{
     "generate",
     "generateblock",
     "getaddednodeinfo",
+    "getaddressbalance",
     "getbestblockhash",
     "getblock",
     "getblockchaininfo",
